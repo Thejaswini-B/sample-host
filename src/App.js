458f2b8bfbbfaaaae30766a687d96f73e97bf922
Thejaswini-B <tejaswini.bandaru@ro-one.in>
2024-03-13 20:52:14 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
-import { useEffect } from 'react';
+
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Pages/Signup/Signup";
 import Homepage from './Pages/Home/Home';
 function App() {
